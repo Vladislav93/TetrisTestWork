ea91a69f686292bd31fc9fee7e731dd65feef970
Vladislav93 <slava9307@gmail.com>
2015-09-07 16:55:02 +0400

    интерфейс

---
+++ Source/Interface/RestartLevelNotAccept.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevelNotAccept : RestartLevel {
+
+	void OnClick()
+	{
+		_gameObj = GameObject.FindGameObjectWithTag ("GameController");
+		_script = _gameObj.GetComponent<gameTetrisLogic>();
+
+		_background.SetActive (false);
+		_label .SetActive (false);
+		Time.timeScale = 1;
+
+		_deRestart.SetActive (false);
+		_script.enabled = true;
+	}
+}
+
