b4633289e2879a19e62a46033841c5df7b820881
Vladislav93 <slava9307@gmail.com>
2015-09-07 12:33:56 +0400

    Меню Интерфейса

---
+++ Source/Interface/RestartLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel: Pause {
+	public GameObject _background;
+	public GameObject _label;
+	public GameObject _deRestart;
+
+	private void pauseForRestart()
+	{
+		Time.timeScale = 0;
+		_gameObj = GameObject.FindGameObjectWithTag ("GameController");	
+		_script = _gameObj.GetComponent<gameTetrisLogic> ();
+		_script.enabled = false;
+	}
+
+	public void Restart ()
+	{
+		Application.LoadLevel (0);
+		Time.timeScale = 1;
+	}
+	
+	void OnClick()
+	{
+		pauseForRestart ();
+		_background.SetActive (true);
+		_label .SetActive (true);
+		_deRestart.SetActive (true);
+	
+	}
+}
---
+++ Source/Interface/RestartLevelAccept.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevelAccept : RestartLevel {
+
+	void OnClick()
+	{
+		Restart ();
+	}
+}
